5ba992a642c19d0a3086a06e5d8da7dcef566fec
Mohammad Ashif <Mohammad Ashif>
2024-01-04 16:06:58 +0600

    query added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+const POST = [
+    { id: 1, title: "post 1" },
+    { id: 2, title: "post 2" },
+];
+
 function App() {
+    // useQuery({
+    //   queryKey: ['posts']
+    // })
+    const postQuery = useQuery({
+        queryKey: ["posts"],
+        queryFn: () => wait(1000).then(() => [...POST]),
+    });
 
+    if (postQuery.isLoading) return <h1>Loading...</h1>;
+    if (postQuery.isError) {
+        return <pre>{JSON.stringify(postQuery.error)}</pre>;
+    }
 
-  return (
-    <>
-      <h1>TanStack Query</h1>
-    </>
-  )
+    return <div>
+      {postQuery.data.map(post => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
 }
 
-export default App
+function wait(duration) {
+    return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
+export default App;
